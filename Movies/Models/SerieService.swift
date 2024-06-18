18ebf1862c905d200ed97e6d5ff8d2a1fdefc3ab
Lucas Vizeu <nota10.lucas@gmail.com>
2024-06-18 20:33:02 -0300

    Add SeriesSearch files
    adicionando aquivos e lógica necessária

---
+++ Movies/Models/SerieService.swift
@@ -0,0 +1,52 @@
+//
+//  SerieService.swift
+//  Movies
+//
+//  Created by ios-noite-03 on 18/06/24.
+//
+
+
+import Foundation
+
+struct SeriesService {
+    
+    private let apiBaseURL = "https://www.omdbapi.com/?apikey="
+    private let apiToken = "fad9f001"
+    
+    private var apiURL: String {
+        apiBaseURL + apiToken
+    }
+    
+    private let decoder = JSONDecoder()
+    
+    func searchSeries(withTitle title: String, completion: @escaping ([Series]) -> Void) {
+        let query = title.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+        let endpoint = apiURL + "&s=\(query)&type=series"
+        
+        guard let url = URL(string: endpoint) else {
+            completion([])
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data,
+                  error == nil else {
+                completion([])
+                return
+            }
+            
+            do {
+                let seriesResponse = try decoder.decode(SeriesSearchResponse.self, from: data)
+                let series = seriesResponse.search
+                completion(series)
+            } catch {
+                print("FETCH ALL SERIES ERROR: \(error)")
+                completion([])
+            }
+        }
+        
+        task.resume()
+    }
+}
